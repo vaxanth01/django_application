de7bd23a8b08081b57a592d4de08df21509d4218
vaxanth01 <vaxanth01@gmail.com>
2024-06-19 23:53:36 +0530

    to host process

--- vasanth_portfolio/settings.py
+++ vasanth_portfolio/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&xgccoesz+b)pxxx(eefhmc97mpdzsu6fviuk-pvcyj%9j$d)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
