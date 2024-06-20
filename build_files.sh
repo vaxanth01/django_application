52e72bf4c93f56b9f170ad68c2693376d6ea8056
vaxanth01 <vaxanth01@gmail.com>
2024-06-20 10:52:31 +0530

    changes by vasanth

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+#!/bin/bash
+
+# Ensure the correct Python environment is used
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput
