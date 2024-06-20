d93a01ebc34339e8457cbcccc62cc9acbf7e9f15
vaxanth01 <vaxanth01@gmail.com>
2024-06-20 11:34:24 +0530

    yse

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,2 @@
-#!/bin/bash
-
-# Ensure Python and pip are installed
-apt-get update
-apt-get install -y python3 python3-pip
-
-# Install project dependencies
-pip3 install -r requirements.txt
-
-# Collect static files
-python3 manage.py collectstatic --noinput
-
-
-chmod +x build_files.sh
+pip install -r requirements.txt
+python manage.py collectstatic
